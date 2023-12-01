424e6160b153e13eef41241d0da5d9f60b84ff6b
Akulov Kirill <113677480+KirAuki@users.noreply.github.com>
2023-12-01 20:53:33 +0300

    axios , pagination

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -1,68 +1,51 @@
-import React from 'react';
-import { Space, Table, Tag } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Table, Button } from 'antd';
 import type { ColumnsType } from 'antd/es/table';
+import axios from 'axios';
 
 interface DataType {
-  key: string;
   name: string;
-  age: number;
-  address: string;
-  tags: string[];
+  country: string;
 }
 
 const columns: ColumnsType<DataType> = [
   {
-    title: 'Name',
+    title: 'Страна',
+    dataIndex: 'country',
+    key: 'country',
+    render: (text) => <a >{text}</a>,
+  },
+  {
+    title: 'Название школы',
     dataIndex: 'name',
     key: 'name',
-    render: (text) => <a>{text}</a>,
   },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (_, { tags }) => (
+];
+
+function TableComponent() {
+  const LIMIT_LIST_SCHOOL = 10;
+  const [page, setPage] = useState<number>(1)
+  const [dataSource, setDataSource] = useState<DataType[]>()
+  const getUniversity = async (page: number, limit: number) => {
+      const response = await axios.get(`http://universities.hipolabs.com/search?offset=${page*limit}&limit=${limit}`)
+      setDataSource(response.data);    
+  }
+  useEffect(() => {
+      getUniversity(page, LIMIT_LIST_SCHOOL)
+  }, [page])
+
+  return (
       <>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
+          <Table dataSource={dataSource} columns={columns} pagination={false}/>
+          <div className='table__page-buttons'>
+              <Button onClick={() => setPage(page - 1)} disabled={!(page-1)}>Назад</Button>
+              <p className='table__page-count'>{page}</p>
+              <Button onClick={() => setPage(page + 1)}>Вперед</Button>
+          </div>
       </>
-    ),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (_, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
+  )
+}
 
-const data: DataType[] = [
-  
-];
 
-const App: React.FC = () => <Table columns={columns} dataSource={data} />;
 
-export default App;
+export default TableComponent;

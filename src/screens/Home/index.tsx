4dea10b1582d6af254d5aa29724869ea03d91fad
Akulov Kirill <113677480+KirAuki@users.noreply.github.com>
2024-01-04 17:59:21 +0300

    error

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,7 +6,7 @@ const Home : FC = () => {
     return (
         <div className="screen-container">
                 <h1>Welcome to Spotify Clone</h1>
-                <p>all tracks 30sec))</p>  
+                <p>Sorry all tracks 30sec))</p>  
         </div>
     )
 }

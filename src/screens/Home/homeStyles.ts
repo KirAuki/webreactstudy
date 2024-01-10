3f9b55240756d9ba229426dd619a69dab74f5c43
Akulov Kirill <113677480+KirAuki@users.noreply.github.com>
2024-01-10 16:31:04 +0300

    form and video

---
+++ src/screens/Home/homeStyles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import { Form, Input, Button, Card, Space } from 'antd';
+
+
+export const CustomForm = styled(Form)`
+    width: 400px;
+`;
+
+export const CustomFormItem = styled(Form.Item)`
+    width: 100%;
+    margin-bottom: 16px;
+    & .ant-form-item-label >label {
+        color: var(--theme-first-color);
+    }
+    
+`;
+
+export const StyledInput = styled(Input)`
+    background-color: var(--theme-bg);
+    & .ant-input {
+        color: var(--theme-first-color);
+        background-color: transparent;
+    }
+`;
+
+export const StyledTextArea = styled(Input.TextArea)`
+    background-color: var(--theme-bg);
+    & .ant-input {
+        color: var(--theme-first-color);
+        background-color: transparent;
+    }
+`;
+
+
+export const FeedbackCard = styled(Card)`
+    width: 400px;
+    background: var(--theme-bg);
+    color: var(--theme-first-color);
+`;
+
+export const CustomButton = styled(Button)`
+    box-shadow: none;
+`;

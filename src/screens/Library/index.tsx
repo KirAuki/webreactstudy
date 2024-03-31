fc7abd5251512804e6bf2ab56e0d0cfb7a68c729
Akulov Kirill <113677480+KirAuki@users.noreply.github.com>
2024-03-31 20:26:36 +0300

    Реализовал динамическую пагинацию

--- src/screens/Library/index.tsx
+++ src/screens/Library/index.tsx
@@ -1,8 +1,9 @@
-import React, { FC, useState, useEffect } from 'react';
-import { Col, Row } from 'antd';
-import { useNavigate } from 'react-router-dom';
-import APIKit from '../../spotify';
-import * as S from "./libraryStyle"
+import React, { FC, useState, useEffect } from "react";
+import { Col, Row } from "antd";
+import { useNavigate } from "react-router-dom";
+import APIKit from "../../spotify";
+import { useInView } from "react-intersection-observer";
+import * as S from "./libraryStyle";
 
 interface Playlist {
   id: number;
@@ -17,51 +18,70 @@ const Library: FC = () => {
   const playlistsPerPage = 6;
 
   useEffect(() => {
-    APIKit.get('me/playlists').then((response) => {
-      setPlaylists(response.data.items);
-    });
-  }, []);
+    loadPlaylists();
+  }, [currentPage]);
 
   const navigate = useNavigate();
 
-  const playPlaylist = (id: number) => {
-    navigate('/player', { state: { id: id } });
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      loadPlaylists();
+    }
+  }, [inView]);
+
+  const loadPlaylists = () => {
+    APIKit.get("me/playlists", {
+      params: { offset: (currentPage - 1) * playlistsPerPage },
+    }).then((response) => {
+      setPlaylists((prevPlaylists) => {
+        return prevPlaylists
+          ? [...prevPlaylists, ...response.data.items]
+          : response.data.items;
+      });
+    });
   };
 
-  const indexOfLastPlaylist = currentPage * playlistsPerPage;
-  const indexOfFirstPlaylist = indexOfLastPlaylist - playlistsPerPage;
-  const currentPlaylists = playlists?.slice(indexOfFirstPlaylist, indexOfLastPlaylist) || [];
-
-  const paginate = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
+  const playPlaylist = (id: number) => {
+    navigate("/player", { state: { id: id } });
   };
 
   return (
     <div className="screen-container">
-      <Row style={{ margin: '0' }} gutter={16}>
-          {currentPlaylists.map((playlist: any) => (
-              <Col span={8} key={playlist.id}>
-                  <S.CustomCard
-                      title={playlist.name}
-                      onClick={() => playPlaylist(playlist.id)}
-                      style={{ marginBottom: 16 }}
-                  >
-                      {playlist.images && playlist.images.length > 0 && (
-                        <img src={playlist.images[0].url} alt={playlist.name}/>
-                      )}
-                      <p>Треков: {playlist.tracks.total}</p>
-                  </S.CustomCard>
-              </Col>
-          ))}
+      <Row style={{ margin: "0" }} gutter={16}>
+        {playlists?.map((playlist: any, index) => (
+          <Col span={8} key={playlist.id}>
+            {index === playlists.length - 1 ? (
+              <div ref={ref}>
+                <S.CustomCard
+                  title={playlist.name}
+                  onClick={() => playPlaylist(playlist.id)}
+                  style={{ marginBottom: 16 }}
+                >
+                  {playlist.images && playlist.images.length > 0 && (
+                    <img src={playlist.images[0].url} alt={playlist.name} />
+                  )}
+                  <p>Треков: {playlist.tracks.total}</p>
+                </S.CustomCard>
+              </div>
+            ) : (
+              <S.CustomCard
+                title={playlist.name}
+                onClick={() => playPlaylist(playlist.id)}
+                style={{ marginBottom: 16 }}
+              >
+                {playlist.images && playlist.images.length > 0 && (
+                  <img src={playlist.images[0].url} alt={playlist.name} />
+                )}
+                <p>Треков: {playlist.tracks.total}</p>
+              </S.CustomCard>
+            )}
+          </Col>
+        ))}
       </Row>
-      <div style={{ textAlign: 'center', marginTop: 16 }}>
-        <S.CustomPagination
-          current={currentPage}
-          total={playlists?.length || 0}
-          pageSize={playlistsPerPage}
-          onChange={paginate}
-        />
-      </div>
     </div>
   );
 };
